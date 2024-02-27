9daa35deeb7a761bbf5704b3f939a1e974664bb8
Juan Carlos <animashorse3@yahoo.com.mx>
2024-02-27 13:30:25 -0600

    Cambios de imágenes y sección de coming soon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'home', redirectTo: '' },
-  { path: 'contact', component: ContactComponent }
+  { path: '', redirectTo: '/coming-soon', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'coming-soon', component: ComingSoonComponent }
 ];
