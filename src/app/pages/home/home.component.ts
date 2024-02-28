1ab2e320ae22f6e7c0290248d7b8e61e5b715b28
Juan Carlos <animashorse3@yahoo.com.mx>
2024-02-28 13:56:21 -0600

    Animaciones AOS Landing Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common'; // AOS
+import AOS from 'aos'; 
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  title = 'LaCamDeAlan - Home';
 
+  // AOS
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  // AOS
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
 }
