98c0c6d3987cd6af2a8a45bff91ce65d4d69ed35
Juan Carlos <animashorse3@yahoo.com.mx>
2024-02-28 12:02:53 -0600

    Landing responsiva

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/coming-soon', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'contact', component: ContactComponent },
   { path: 'coming-soon', component: ComingSoonComponent }
 ];
